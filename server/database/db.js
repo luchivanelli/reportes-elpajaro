81830e03710ef477fd5dd44bad176862a118a3c1
luchivanelli <lucianavanelli13@gmail.com>
2025-08-18 18:23:13 -0300

    arreglado errores

--- server/database/db.js
+++ server/database/db.js
@@ -1,4 +1,5 @@
 import mysql2 from 'mysql2';
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,9 +11,22 @@ dotenv.config();
 //     database: process.env.DB_NAME,
 // });
 
-export const connection = mysql2.createConnection({
-    host: "bpa1yxhjhwbhnuhuzze4-mysql.services.clever-cloud.com",
-    user: "unhiiswmbyubs4bl",
-    password: "ijgEm88w3fV3SXNDwkF5",
-    database: "bpa1yxhjhwbhnuhuzze4",
+// export const connection = mysql2.createConnection({
+//     host: "bpa1yxhjhwbhnuhuzze4-mysql.services.clever-cloud.com",
+//     user: "unhiiswmbyubs4bl",
+//     password: "ijgEm88w3fV3SXNDwkF5",
+//     database: "bpa1yxhjhwbhnuhuzze4",
+// });
+
+
+const pool = mysql.createPool({
+  host: "bpa1yxhjhwbhnuhuzze4-mysql.services.clever-cloud.com",
+  user: "unhiiswmbyubs4bl",
+  password: "ijgEm88w3fV3SXNDwkF5",
+  database: "bpa1yxhjhwbhnuhuzze4",
+  waitForConnections: true,
+  connectionLimit: 5, // no más de 5 porque es tu límite
+  queueLimit: 0
 });
+
+export default pool;
